8f7335ef2382d14006d188a1b22757f131dc29bc
GechKibr <getachewkibr1995@gmail.com>
2025-11-25 20:08:41 +0000

    footer page modified

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <p>&copy; 2024 Corruption Reporting System. All rights reserved.</p>
+        <p>&copy; 2025 Corruption Reporting System. All rights reserved.</p>
       </div>
     </footer>
   );
